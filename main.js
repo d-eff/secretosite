1e17f7c76ede73fc49d5c9cdd4f5d71e374906af
davidf <dfernandez06@gmail.com>
2026-01-03 23:30:17 -0500

    Eyes v0.5

--- main.js
+++ main.js
@@ -22,6 +22,44 @@ function drawLine(ctx, startX, startY, endX, endY) {
   ctx.stroke();
 }
 
+function clamp(val, min, max) {
+  return Math.min(Math.max(val, min), max);
+}
+
+function getPupilPos(clientXpos, clientYpos, ellipseXPos, ellipseYPos, ellipseXLength, ellipseYLength) {
+  const clientX = clientXpos - ellipseXPos;
+  const clientY = clientYpos - ellipseYPos;
+  const angle = Math.atan(clientX/clientY);
+  const xAng = ellipseXLength * Math.sin(angle);
+  const yAng = ellipseYLength * Math.cos(angle);
+  let x = ellipseXPos;
+  let y = ellipseYPos;
+  if(clientX >= 0 && clientY >= 0) {
+    //BOTTOM RIGHT
+    x = clamp(clientX, 0, xAng) + ellipseXPos;
+    y = clamp(clientY, 0, yAng) + ellipseYPos;
+  } else if (clientX < 0 && clientY >= 0) {
+    //BOTTOM LEFT
+    x = clamp(clientX, xAng, 0) + ellipseXPos;
+    y = clamp(clientY, 0, yAng) + ellipseYPos;
+  } else if(clientX >= 0 && clientY < 0) {
+    //TOP RIGHT
+    x = clamp(clientX, 0, -xAng) + ellipseXPos;
+    y = clamp(clientY, -yAng, 0) + ellipseYPos;
+  } else if(clientX < 0 && clientY < 0) {
+    //TOP LEFT
+    x = clamp(clientX, -xAng, 0) + ellipseXPos;
+    y = clamp(clientY, -yAng, 0) + ellipseYPos;
+  }
+  return {x, y};
+}
+
+function drawEye(ctx, e, ellipseXPos, ellipseYPos, ellipseXLength, ellipseYLength) {
+  const {x, y} = getPupilPos(e.clientX, e.clientY, ellipseXPos, ellipseYPos, ellipseXLength, ellipseYLength);
+  ctx.clearRect(ellipseXPos - (25 + ellipseXLength), ellipseYPos - (25 + ellipseYLength), ellipseXPos + ellipseXLength, ellipseYPos + ellipseYLength);
+  drawEllipse(ctx, ellipseXPos, ellipseYPos);
+  drawCircle(ctx, x, y)
+}
 
 function main() {
   const canvas = document.querySelector('#secreto')
@@ -32,73 +70,16 @@ function main() {
   let ctx = canvas.getContext('2d');
 
   drawEllipse(ctx, 300, 300);
-  // drawEllipse(ctx, 500, 300);
+  drawEllipse(ctx, 500, 300);
 
   drawCircle(ctx, 300, 300);
-  // drawCircle(ctx, 500, 300);
-
+  drawCircle(ctx, 500, 300);
   
   canvas.addEventListener('mousemove', (e) => {
-    //if y > 
-    // 300,300 is center of ellipse
-    const ellipseXPos = 300;
-    const ellipseYPos = 300;
-    const ellipseXLength = 75;
-    const ellipseYLength = 50;
-    // don't need to offset from rect.left/rect.top bc it's always fullscreen
-    const clientX = e.clientX - ellipseXPos;
-    const clientY = e.clientY - ellipseYPos;
-    const angle = Math.atan(clientX/clientY);
-    const xAng = ellipseXLength * Math.sin(angle);
-    const yAng = ellipseYLength * Math.cos(angle);
-    // const y = yAng + 300
-    let x = ellipseXPos;
-    let y =ellipseYPos;
-    // const x = clientX > ellipseXLength ? ellipseXPos + xAng : ellipseXPos - xAng;
-    // const y = clientY < ellipseYLength ? ellipseYPos - yAng : yAng + ellipseYPos;
-
-    if(clientX > 0 && clientY > 0) {
-      //BOTTOM RIGHT
-      // console.log("BOTTOM RIGHT")
-      x = 300 + xAng;
-      y = 300 + yAng;
-    } else if (clientX < 0 && clientY > 0) {
-      //BOTTOM LEFT
-      // console.log("BOTTOM LEFT")
-      x = 300 + xAng;
-      y = 300 + yAng;
-    } else if(clientX > 0 && clientY < 0) {
-      // console.log("TOP RIGHT");
-      x = 300 - xAng;
-      y = 300 - yAng;
-    } else if(clientX < 0 && clientY < 0) {
-      // console.log("TOP LEFT")
-      x = 300 - xAng;
-      y = 300 - yAng;
-    } else {
-      x = 300;
-      y = 300;
-    }
-    // console.log('----------')
-    // console.log("clientleft, clienttop:", clientX, clientY);
-    // console.log("xr, yr:", xAng, yAng)
-    
-    // console.log("compX, compY", x, y)
-    // console.log("x, y:", x, y)
-    
-    // const xMax = 300 + 75
-    // const xMin = 300 - 75;
-    // const yMax = 300 + 50;
-    // const yMin = 300 - 50;
-    
-    // clamp - Math.min(Math.max(Number, min), max);
-    // const xPos = Math.min(Math.max(left, xMin), xMax);
-    // const yPos = Math.min(Math.max(top, yMin), yMax);
-    ctx.clearRect(0, 0, rect.width, rect.height);
-    
-    drawEllipse(ctx, 300, 300);
-    drawLine(ctx, 300, 300, clientX + 300, clientY + 300);
-    drawCircle(ctx, x, y)
+    //left - 291, 370, 33.5, 20.5
+    // right - 466, 372, 36.5, 22
+    drawEye(ctx, e, 300, 300, 75, 50)
+    drawEye(ctx, e, 500, 300, 75, 50)
   });
 
   window.addEventListener('resize', () => {
