5e9a4126ef6200561b2106076b9372df906b1913
d-eff <d-eff@users.noreply.github.com>
2026-02-04 04:47:48 +0000

    deploy: 0cb2208f3682ccba11df2813b9acea3978db0d55

--- scripts/ads.js
+++ scripts/ads.js
@@ -1,17 +1,19 @@
 const banners = [
   { src: "bookchains.png", alt: "Buy bookchains now, before your books escape." },
   { src: "moonshoes.png", alt: "PUT ON THE MOON SHOES, SHINJI." },
+  { src: "painting.png", alt: "A little dick paint, as a treat." },
   { src: "pigsblood.png", alt: "We don\'t know why you want it. We\'re not here to judge. Consume." },
   { src: "spiders.png", alt: "With apoloies to Rachel." },
   { src: "todd.png", alt: "You know you want it." },
-  { src: "trucknuts.png", alt: "Eww, look at that truck with those dry, cracked nuts." }
+  { src: "trucknuts.png", alt: "Eww, look at that truck with those dry, cracked nuts." },
+  { src: "wiserd.png", alt: "WISERD" }
 ]
 
 const leftBanner = document.getElementById('leftBanner');
 const rightBanner = document.getElementById('rightBanner');
 
-const leftIndex = Math.floor(Math.random() * 6);
-let rightIndex = Math.floor(Math.random() * 6);
+const leftIndex = Math.floor(Math.random() * banners.length);
+let rightIndex = Math.floor(Math.random() * banners.length);
 
 while(leftIndex === rightIndex) {
   rightIndex = Math.floor(Math.random() * 6);
